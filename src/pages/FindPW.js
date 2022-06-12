27c018a36ed22e68a90da08e972e27461668d170
(sehun) <(dkstpgns95@naver.com)>
2022-06-12 17:23:03 +0900

    패스워드찾기 1차수정

--- src/pages/FindPW.js
+++ src/pages/FindPW.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -15,22 +16,23 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 
 const FindPW = () => {
+    const navigate = useNavigate();
+    const [disable, setDisable] = useState(true);
+    const [eMail, setEm] = useState('');
+    const [isErr, setIsErr] = useState(false);
+    const [step, setStep] = useState("findPW");
 
-
-    /*const [value, setValue] = useState("");
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
+    const handleInput = (event) => {
+        return event.target.value;
     };
 
-    useEffect(() => {
-        console.log("value: " + value)
-    }, [value])*/
-
-
-
-
-
+    const handleDisable = () => {
+        if (eMail.includes('@')) {
+            setDisable(false);
+        } else {
+            setDisable(true);
+        }
+    };
 
 
 
@@ -46,17 +48,42 @@ const FindPW = () => {
                         <Typography sx={{ fontSize: 20 }} color="black" >
                             비밀번호 찾기
                         </Typography>
+                        <Typography sx={{ fontSize: 12 }} color="gray" variant="h1" component="div">
+                            회원가입 시 입력한 이메일 입력해주세요.
+                        </Typography>
 
                         <Box>
-                            <TextField className="Id" label="이메일" name="email" fullWidth autoFocus sx={{ mt: 3 }} />
-                            <br />
-                            <TextField className="Password" label="비밀번호" name="password" fullWidth type="password" sx={{ mt: 2 }} />
-
+                            <TextField
+                                label="이메일"
+                                name="Email"
+                                fullWidth
+                                autoFocus
+                                sx={{ mt: 3 }}
+                                error={isErr}
+                                helperText={isErr ? "입력한 정보와 일치하는 계정이 없습니다." : ""}
+                                onChange={event => {
+                                    setEm(handleInput(event));
+                                    handleDisable();
+                                }} />
                         </Box>
 
-                        <Button type="submit" variant="contained" fullWidth sx={{ mt: 3 }} disabled>
+                        <Button type="submit" variant="contained" fullWidth sx={{ mt: 3 }} disabled={disable}>
                             비밀번호 찾기
                         </Button>
+
+
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            fullWidth
+                            onClick={
+                                () => {
+                                    setIsErr(true)
+                                }
+                            }>
+                            에러발생
+                        </Button>
+
                     </CardContent>
                 </Card>
             </Container>
