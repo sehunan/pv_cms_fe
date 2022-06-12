21fb3e6e55c6d01eb902f46ce0e24527548f9d06
(sehun) <(dkstpgns95@naver.com)>
2022-06-12 16:30:30 +0900

    아이디 찾기 추가

--- src/pages/FindID.js
+++ src/pages/FindID.js
@@ -19,6 +19,7 @@ const FindID = () => {
     const [disable, setDisable] = useState(true);
     const [phoneNumber, setPh] = useState('');
     const [isErr, setIsErr] = useState(false);
+    const [step, setStep] = useState("findID");
 
     const handleInput = (event) => {
         return event.target.value;
@@ -33,58 +34,103 @@ const FindID = () => {
     };
 
     return (
-        <>
-            <Container component="main" maxWidth="xs">
-                <Card sx={{
-                    marginTop: 35,
-                    flexDirection: "column",
-                    alignItems: "center",
-                    boxShadow: "7"
-                }}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 20 }} color="black" >
-                            아이디 찾기
-                        </Typography>
+        <Container component="main" maxWidth="xs">
 
-                        <Typography sx={{ fontSize: 12 }} color="gray" variant="h1" component="div">
-                            회원가입 시 입력한 핸드폰 번호(숫자 11자리)를 입력해주세요.
-                        </Typography>
+            <Card sx={{
+                marginTop: 35,
+                flexDirection: "column",
+                alignItems: "center",
+                boxShadow: "7"
+            }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="black" >
+                        아이디 찾기
+                    </Typography>
 
-                        <Box>
-                            <TextField
-                                label="핸드폰 번호"
-                                name="phoneNumber"
+                    <Typography sx={{ fontSize: 12 }} color="gray" variant="h1" component="div">
+                        회원가입 시 입력한 핸드폰 번호(숫자 11자리)를 입력해주세요.
+                    </Typography>
+
+                    {
+                        step == "findID" &&
+                        <>
+                            <Box>
+                                <TextField
+                                    label="핸드폰 번호"
+                                    name="phoneNumber"
+                                    fullWidth
+                                    autoFocus
+                                    sx={{ mt: 3 }}
+                                    error={isErr}
+                                    helperText={isErr ? "입력한 정보와 일치하는 계정이 없습니다." : ""}
+                                    onChange={event => {
+                                        setPh(handleInput(event));
+                                        handleDisable();
+                                    }} />
+                            </Box>
+
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                fullWidth sx={{ mt: 3 }}
+                                disabled={disable}
+                                onClick={
+                                    () => {
+                                        setStep("showID")
+                                    }
+                                }>
+                                확인
+                            </Button>
+
+                            <Button
+                                type="submit"
+                                variant="contained"
                                 fullWidth
-                                autoFocus
+                                onClick={
+                                    () => {
+                                        setIsErr(true)
+                                    }
+                                }>
+                                에러발생
+                            </Button>
+                        </>
+
+                    }
+                    {
+                        step == "showID" &&
+                        <>
+                            <Box>
+                                <TextField
+                                    label="이메일"
+                                    name="Findmail"
+                                    defaultValue="seh*****@naver.com"
+                                    InputProps={{
+                                        readOnly: true,
+                                    }}
+                                    fullWidth
+                                    autoFocus
+                                    sx={{ mt: 3 }}
+                                />
+                            </Box>
+                            <Button
+                                type="submit"
+                                variant="contained"
                                 sx={{ mt: 3 }}
-                                error={isErr}
-                                helperText={isErr ? "입력한 정보와 일치하는 계정이 없습니다." : ""}
-                                onChange={event => {
-                                    setPh(handleInput(event));
-                                    handleDisable();
-                                }} />
-                        </Box>
-
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            fullWidth sx={{ mt: 3 }}
-                            disabled={disable}>
-                            확인
-                        </Button>
-
-                        <Button type="submit" variant="contained" fullWidth onClick={
-                            () => {
-                                setIsErr(true)
-                            }
-                        }>
-                            에러발생
-                        </Button>
-
-                    </CardContent>
-                </Card>
-            </Container>
-        </>
+                            >
+                                로그인 하기
+                            </Button>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                sx={{ mt: 3 }}
+                            >
+                                비밀번호 찾기
+                            </Button>
+                        </>
+                    }
+                </CardContent>
+            </Card>
+        </Container>
     )
 };
 
